de578533bc329d00abd500537816c8c9e6129b86
James <andritjm@gmail.com>
2022-09-12 19:37:12 -0500

    pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log('Yo!')
+// define array for choices [r, p, s]
+// define var for computerChoice
+// define var for playerChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+
+// prompt playerChoice
+
+// randomly choose computerChoice
+
+// compare choices
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+// ask to play again?
+    //restart game
+// else
+    // end game
