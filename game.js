e6e3f17949c59e47ff6db2e5e22605fdfb760813
James <andritjm@gmail.com>
2022-09-12 21:51:03 -0500

    tried to keep up with Josh, so here's most of it

--- game.js
+++ game.js
@@ -1,22 +1,53 @@
 // define array for choices [r, p, s]
-// define var for computerChoice
-// define var for playerChoice
-// define var for wins
-// define var for losses
-// define var for ties
+var choices = ['r', 'p', 's']
 
 
-// prompt playerChoice
+var wins = 0
+var losses = 0
+var ties = 0
 
-// randomly choose computerChoice
+var keepPlaying = true
 
-// compare choices
+while(keepPlaying){
 
-// display (alert) comparison results (won, tied, lost round)
+    // prompt humanChoice
+    var humanChoice = prompt("Choose r, p, s")
+    console.log('human choice:', humanChoice)
+    
+    // randomly choose computerChoice
+    var random = Math.floor(Math.random() * choices.length)
+    var computerChoice = choices[random]
+    console.log('computer choice:', computerChoice)
+    
+    var result
+    if (humanChoice === computerChoice){
+        ties ++
+        result = "tied!"
+    } else if (
+        humanChoice === "r" && computerChoice === "s" ||
+        humanChoice === "p" && computerChoice === "r" ||
+        humanChoice === "s" && computerChoice === "p"
+    ){
+        wins++
+        result = "won!"
+    } else {
+        losses++
+        result = "lost!"
+    }
+    
+    
+    // display (alert) comparison results (won, tied, lost round)
+    alert("You " + result)
+    
+    // show stats
+    alert("Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
+    
+    // ask to play again?
+    var playAgain = confirm("Want to play again?")
+    if (playAgain) {
 
-// show stats (number of wins, losses, ties)
-
-// ask to play again?
-    //restart game
-// else
-    // end game
+    }
+        //restart game
+    // else
+        // end game
+}
